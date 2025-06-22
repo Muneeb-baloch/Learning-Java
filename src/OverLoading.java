9a1fd315ae4d61056149b713727dda10d18b4f74
Muneeb-baloch <muneebmultan1999@gmail.com>
2025-06-22 11:46:01 +0500

    Overloading concept in java.

--- src/OverLoading.java
+++ src/OverLoading.java
@@ -1,10 +1,10 @@
-public class Overloading {
+public class OverLoading {
 // Method overloading means same name method but with different parameters
     // like we have in this short program add. 2 add methods are here and one accept
     // 3 parameters and other is accepting 2 parameters.
     public static void main(String[] args) {
-        Overloading obj = new Overloading();
-        Overloading.Calculator obj2 = obj.new Calculator(); // here we are using Nested Class concept
+        OverLoading obj = new OverLoading();
+        OverLoading.Calculator obj2 = obj.new Calculator(); // here we are using Nested Class concept
        int r =  obj2.add(5,15);
         System.out.println(r);
 
